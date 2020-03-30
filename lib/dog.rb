c6554b241e39b07d9cab25b29ba047b24abe6e74
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-30 20:29:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,6 @@ class Dog
   def initialize(name)
     @name = name
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  def initialize(breed)
+    @breed = breed 
 end
